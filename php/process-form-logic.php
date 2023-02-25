ec031af3ea481ebd1db6a81cfc6816126817fe4a
Jumma <jumma.ahmed1543@gmail.com~>
2023-02-25 19:44:30 +0000

    comments_andimprovments

--- php/process-form-logic.php
+++ php/process-form-logic.php
@@ -6,11 +6,31 @@
 
     }
 
-    //MAKE SURE THAT THE FILE ARRAY IS NOT EMPY 
+    //MAKE SURE THAT THE FILE ARRAY IS CORRECTLY FORMATTED
     if(empty($_FILES)){
         exit("Ops, seems like the files was not selected properly!"); 
     }
-    
+    //IF THE FILE ARRAY IS NOT CORRECT
+    if($_FILES["image"]["error"] !== UPLOAD_ERR_OK){
+        switch($_FILES["image"]["error"]){
+            //CHECK FOR PARTIALLY UPLOADED FILE
+            case UPLOAD_ERR_PARTIAL:
+                exit("Ops seems like upload was not correctly done."); 
+                break; 
+            case UPLOAD_ERR_NO_FILE:
+                exit("Ops, seems like you have not submitted anything!."); 
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                exit("Ops, there is a conflicting extension."); 
+                break;
+            case UPLOAD_ERR_FORM_SIZE: 
+                exit("Ops, file is too large!");
+                break; 
+            default:
+                exit("Ops, I cant figure out the error!"); 
+                break; 
+        }
+    }
     //VIEW FILE ARRAY 
     print_r($_FILES);
 ?>
