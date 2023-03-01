16a83a519fe476605f6ce53aa623e0e3ee3ab39e
Jumma <jumma.ahmed1543@gmail.com~>
2023-03-01 17:06:23 +0000

    connection!

--- php/connectToDatabase.php
+++ php/connectToDatabase.php
@@ -1,6 +1,19 @@
 <?php
     //VARIABALE FOR THE DB SERVER NAME
-    $databaseServerName="localhost"; 
-    $databaseUserName = "root";
-    $db
+    $serverHostName="localhost"; 
+    $serverUserName = "root";
+    $serverPassword=""; 
+    $serverDatabseName = "file_storage_database"; 
+
+    //CONNECT TO DATABASE WHICH WILL RETEUN AN OBJ RAPRESENTING THE DATABASE CONNECTION
+    $connectionObject = mysqli_connect(hostname:$serverHostName, username:$serverUserName, password:$serverPassword, database:$serverDatabseName); 
+
+    //MAKE SURE THAT THE CONNECTION HAS BEEN SUCCESSFUL BY CHECKING WEATHER THE CODE WAS 0
+    if(mysqli_connect_errno()){
+        //STOP THE SCRIPT AND PRINT A MESSAGE 
+        die("Ops, connection error has occured!");
+    }else{
+        //IF THE CONNECTION WAS SUCCESSFUL
+        echo "Connection Estabilished!"; 
+    }
 ?>
