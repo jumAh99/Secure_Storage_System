e2bb782821b94dbab86d7d848aa6920744a8f370
jumAh99 <jumma.ahmed87@gmail.com>
2023-03-19 16:05:59 +0000

    added-useful-comments

--- php/delete-file.php
+++ php/delete-file.php
@@ -7,35 +7,38 @@ REQUIRE_ONCE 'connect-to-database.php';
 REQUIRE_ONCE 'file-sharing-util.php'; 
 //CHECK WEATHER THE FILE IS SELECTED CORRECTLY AND ITS VALID
 if(!empty($_GET['file'])){
-    //GET THE FILE AND STORE IT INTO A LOCAL VARIABLE
+    //EXTRACT THE FILE INFORMATION FROM THE URL
     $fileName = basename($_GET['file']);
-    // GET THE USER ID FROPM THE CURRENT SESSION
+    //GET THE USER ID FROM THE CURRENT SESSION
     $userID = $_SESSION["userID"];
-    //DELETE THE SHARED RECORD 
+    //GET THE FILE INFORMATION FROM THE DATABASE USING THE USER ID
     $currentFileInfo = getFileInformationByName($connectionObject, $fileName, $userID);
+    //GET THE USER INFROMATION FROM THE FILE USING THE USER ID
     $getCurrentReceiverFileInfo = getUserInformationByID($connectionObject, $currentFileInfo["userID"]); 
-    //GET THE RECIVER INFORMATION IF NOT OWNER 
-    //EXTRACT THE NECESSARY INFORMATION INTO LOCAL VARIABLES
+    //GET THE CURRENT OWNER OF THE FILE
     $fileOwner = $currentFileInfo["Owner"]; 
-    //GET THE VALUE OF THE FILE BY OWNER 
+    //GET THE SHARING RECORDS FORM THE SHARE TABLE
     $currentShareFileInfo = getCurrentFileValue($connectionObject, $fileName, $fileOwner, $getCurrentReceiverFileInfo["userUID"]); 
-    // GET THE PATH OF THE FILE PRESENT IN THE SERVER
+    //GET THE PATH OF THE OWNER FILE LAOCATION
     $ownerEncryptedDestinationPathUser = __DIR__ . "/../uploads/" . $_SESSION["userUID"] . "/encrypted/" .  $fileName;  
+    //GET THE CORRECT FILR FILE NAME IMPLEMENTATION 
     $fileNameSensor = $_SESSION['userUID'] ."-". $fileName; 
-    $fileReceiver = $currentShareFileInfo["receiverUID"]; 
-    //IF THE USER IS THE OWNER THEN DO THE FOLLOWING
+    //GET THE RECEIVER OF THE CURRENT FILE INFORMATION 
+    $fileReceiver = $currentShareFileInfo["receiverUID"];
+    //IF THE USER IS THE OWNER OF THE FILE THEN DELETE ALL THE FILES WHAT WERE SHARED ELSE JUST THE CURRENT FILE AND THE SHARE RECORD 
     if($_SESSION["userUID"] == $currentShareFileInfo["senderUID"]){
+        //DELETE ALL THE SHARED RECORD ASSOCIATED WITH THE FILE
         deleteSharedRecord($connectionObject, $fileNameSensor, $_SESSION['userUID']); 
+        //DELETE THE DILES PRESENT IN THE RECEIVER DIRECTORIES
         deleteFilesDetailsOfTheOwner($connectionObject, $fileNameSensor, $fileOwner);  
     }else if($_SESSION["userUID"] == $currentShareFileInfo["receiverUID"]){
-        //DELETE THE SHARE RECORD FOROM THE TABLE
+        //JUST DELETE THE FILE RECORD ASSOCIATED WITH THE RECEIVVER
         deleteSingleSharedRecord($connectionObject, $fileName, $_SESSION['userUID']);
     }
-    //CALL THE FUNCTION TO DELETE THE FILE
+    //DELETE THE FILE THAT IS PRESENT IN THIS DIRECTORY
     deleteFile($connectionObject, $fileName, $userID, $ownerEncryptedDestinationPathUser); 
-    //ADD THE ERROR TYPE TO URL SO WE CAN USE THAT AS A MESSAGE
+    //SEND BACK THE USER TO FORM PAGE
     header("location: ../form?error=fileDeleted");
-    header("location: ../form?error=$fileName&receiver=$fileReceiver");
     exit(); 
 
 }else{
