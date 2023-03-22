b4fabd3977b8555c766158e9cc001dce30e8c289
jumAh99 <jumma.ahmed87@gmail.com>
2023-03-22 12:47:48 +0000

    added-welcome-message

--- form.php
+++ form.php
@@ -8,6 +8,12 @@
             //IF THE SESSION IS SET THEN THE PAGE WILL SHOW THE REQUIRED FUNCTIONALITIES
             if(isset($_SESSION["userID"])){
                 REQUIRE_ONCE "template-html-components/form-session-is-set.php"; 
+            }else{
+                echo "<div class=\"welcome-message\"> <h1>Welcome to my Secure Storage, this project has been undergoing development for a long time,  
+                 has been entirely programmed by myself with the goal to offer anyone,with a reliable way to store their precious files. Note that the application is still in it's infancy and improvment and enchancements
+                 are undergoing everyday. I would really appriciate it if you gave it it a try and <a href=\"login\">logged</a> in to give it a try, 
+                 any feedbacks that you might offer is appricated.
+                 Thank you for your time and I hope you have a nice experience with my creation!</h1></div>";
             }
         ?>
     </div>
