4d712626085fc549c3f7e84170fe309c1f7744c8
jumAh99 <jumma.ahmed87@gmail.com>
2023-03-22 16:01:50 +0000

    fixes

--- template-html-components/welcome-message.php
+++ template-html-components/welcome-message.php
@@ -1,4 +1,4 @@
 <div class="welcome-message"> <h1>
-Welcome to my Secure Storage, this project has been undergoing development for a long time, has been entirely programmed by myself with the goal to offer anyone,with a reliable way to store their precious files. Note that the application is still in it's infancy and improvment and enchancements
-are undergoing everyday. I would really appriciate it if you gave it it a try and <a href="login">logged</a> in to give it a try, any feedbacks that you might offer is appricated.
+Welcome to my Secure Storage, this project has been undergoing development for a long time, it has been entirely programmed by myself with the goal to offer anyone with a reliable way to store their precious files. Note that the application is still in its infancy and improvement and enhancements
+are undergoing everyday. I would really appreciate it if you gave it a try and <a href="login">logged</a> in to see it for yourself, any feedback that you might offer is appreciated.
 Thank you for your time and I hope you have a nice experience with my creation!</h1></div>
