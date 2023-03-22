aea06d70a7c49e5f8f774a43aa8e04bd39c3822a
jumAh99 <jumma.ahmed87@gmail.com>
2023-03-22 12:56:04 +0000

    made-some-clearification-in -the-code

--- form.php
+++ form.php
@@ -9,11 +9,8 @@
             if(isset($_SESSION["userID"])){
                 REQUIRE_ONCE "template-html-components/form-session-is-set.php"; 
             }else{
-                echo "<div class=\"welcome-message\"> <h1>Welcome to my Secure Storage, this project has been undergoing development for a long time,  
-                 has been entirely programmed by myself with the goal to offer anyone,with a reliable way to store their precious files. Note that the application is still in it's infancy and improvment and enchancements
-                 are undergoing everyday. I would really appriciate it if you gave it it a try and <a href=\"login\">logged</a> in to give it a try, 
-                 any feedbacks that you might offer is appricated.
-                 Thank you for your time and I hope you have a nice experience with my creation!</h1></div>";
+                // JUST A FRIEDNLY WELCOME MESSAGE 
+                REQUIRE_ONCE "template-html-components/welcome-message.php"; 
             }
         ?>
     </div>
---
+++ template-html-components/welcome-message.php
@@ -0,0 +1,4 @@
+<div class="welcome-message"> <h1>
+Welcome to my Secure Storage, this project has been undergoing development for a long time, has been entirely programmed by myself with the goal to offer anyone,with a reliable way to store their precious files. Note that the application is still in it's infancy and improvment and enchancements
+are undergoing everyday. I would really appriciate it if you gave it it a try and <a href="login">logged</a> in to give it a try, any feedbacks that you might offer is appricated.
+Thank you for your time and I hope you have a nice experience with my creation!</h1></div>
